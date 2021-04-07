4d655da3095a0efa371ff97a0e10fd68bd514931
doan <doan@gmail.com>
2021-04-07 16:49:54 +0700

    code

--- Coin/app/src/main/java/com/coin/MainActivity2.java
+++ Coin/app/src/main/java/com/coin/MainActivity2.java
@@ -1,14 +1,14 @@
 package com.coin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
     WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
